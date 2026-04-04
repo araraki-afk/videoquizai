9d90a2cf4aed45e18d0e66390dffda9a605cb17c
araraki-afk <230067@almau.edu.kz>
2026-04-04 22:11:32 +0500

    0.1.1

--- backend/core/security.py
+++ backend/core/security.py
@@ -5,7 +5,7 @@ from core.config import settings
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def hass_password(password: str) -> str:
+def hash_password(password: str) -> str:
     return pwd_context.hash(password)
 
 def verify_password(plain: str, hashed: str) -> bool:
---
+++ backend/models/quiz.py
@@ -0,0 +1,42 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Text, Float, Json
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+from core.database import Base
+
+class Quiz(Base):
+    __tablename__ = "quizzes"
+
+    id = Column(Integer, primary_key=True)
+    content_id = Column(Integer, ForeignKey("contents.id"), nullable=False)
+    title = Column(String, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    content = relationship("Content", back_populates="quizzes")
+    questions = relationship("Question", back_populates="quiz", cascade="all, delete")
+    attempts = relationship("QuizAttempt", back_populates="quiz")
+
+class Question(Base):
+    __tablename__ = "questions"
+
+    id = Column(Integer, primary_key=True)
+    quiz_id = Column(Integer, ForeignKey("quizzes.id"), nullable=False)
+    text = Column(Text, nullable=False)
+    question_type = Column(String, nullable=False) #виды вопросов мультивыбор, открытый, тру/фолс
+    options = Column(Json, nullable=False) #варианты
+    correct_answer = Column(Json, nullable=False)
+    topic_tag = Column(String, nullable=True) #тема из лекции
+
+    quiz = relationship("Quiz", back_populates = "questions")
+
+
+class QuizAttempt(Base):
+    __tablename__ = "quiz_attempts"
+
+    id = Column(Integer, primary_key=True)
+    quiz_id = Column(Integer, ForeignKey("quizzes.id"), nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    score = Column(Float, nullable=False)
+    answers = Column(Json, nullable=False) #ответы пользователя
+    created_at = Column(DateTime(timezone=True),server_default=func.now())
+
+    quiz = relationship("Quiz", back_populates = "questions")
